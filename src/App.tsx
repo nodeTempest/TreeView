6cba48126a39321ca899bff7083b849f58f882a2
Nykolai Topchyi <nodetempest@gmail.com>
2022-04-01 21:06:51 +0300

    implement basic render

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import data from "./data.json";
 import { TreeViewDataType, TreeView } from "./TreeView/TreeView.component";
 
 export const App = () => {
-  return <TreeView data={data as any as TreeViewDataType}>hello</TreeView>;
+  return <TreeView data={data as any as TreeViewDataType} />;
 };
 
 export default App;
--- src/TreeView/TreeView.component.tsx
+++ src/TreeView/TreeView.component.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { StyledTreeViewRoot } from "./TreeView.styled";
 
-export type TreeViewDataType = { title: string; nodes: TreeViewDataType[] }[];
+export type TreeViewDataType = { title: string; nodes: TreeViewDataType }[];
 
 interface ITreeViewProps {
   data: TreeViewDataType;
+  level?: number;
 }
 
-export const TreeView: React.FC<ITreeViewProps> = (props) => {
-  return <StyledTreeViewRoot>hello</StyledTreeViewRoot>;
+export const TreeView: React.FC<ITreeViewProps> = ({ data, level = 0 }) => {
+  return (
+    <StyledTreeViewRoot>
+      {data.map(({ title, nodes }) => (
+        <>
+          <div>
+            <span>{level}</span> {title}
+          </div>
+          <>{!!nodes.length && <TreeView data={nodes} level={level + 1} />}</>
+        </>
+      ))}
+    </StyledTreeViewRoot>
+  );
 };
