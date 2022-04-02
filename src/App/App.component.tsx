a51ad7a285d5cfd7dc8ad9e33e57583239006266
Nykolai Topchyi <nodetempest@gmail.com>
2022-04-02 13:58:43 +0300

    add loadTreeData func

--- src/App/App.component.tsx
+++ src/App/App.component.tsx
@@ -1,7 +1,19 @@
+import React from "react";
 import { StyledApp } from "./App.styled";
 import { TreeViewDataType, TreeView } from "../TreeView/TreeView.component";
 
 import data from "./data.json";
+
+const loadTreeData = async () => {
+  try {
+    const json = await fetch("./data.json");
+    const result: TreeViewDataType = await json.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 export const App = () => {
   return (
